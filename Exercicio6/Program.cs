3d5f29af78e65c6181318a67a1e6fdd8845efb9d
Mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2025-02-18 20:12:23 -0300

    Feat: Exercicios Finalizados

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Metadata;
+
+namespace Exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite quanto você ganha por hora: ");
+            double valorHora = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite quantas horas você trabalhou no mês: ");
+            double horasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+
+            double rendaMensal = valorHora * horasTrabalhadas;
+
+            Console.WriteLine($"{nome}, sua renda mensal é: R$ {rendaMensal:F2}");
+        }
+    }
+}
